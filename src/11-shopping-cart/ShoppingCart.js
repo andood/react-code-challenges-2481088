b302d9fea29fd5d72d5384d7e6f438c3742fd90f
Andrew Hamel <grfxguru@yahoo.com>
2022-11-01 13:05:56 -0500

    Successful solution implemented with useReducer hook.

--- src/11-shopping-cart/ShoppingCart.js
+++ src/11-shopping-cart/ShoppingCart.js
@@ -1,4 +1,9 @@
-import { useState } from 'react'
+import { useReducer } from 'react';
+
+const initialState = {
+  cart: [],
+  total: 0
+}
 
 const items = [{
   name: 'apple',
@@ -11,8 +16,74 @@ const items = [{
   price: 3.99
 }]
 
+function cartReducer (state, action) {
+  const { type, item } = action;
+
+  const calcCartTotal = cart => {
+    const grandTotal = cart.map(item => item.price * item.quantity)
+      .reduce((prev, curr) => {
+        return prev + curr
+      }, 0);
+    return grandTotal.toFixed(2);
+  }
+
+  let updatedCart, cartTotal;
+
+  switch (type) {
+    case "ADD": {
+      if (state.cart.length === 0) {
+        updatedCart = [...state.cart, {...item, quantity: 1}];
+      }
+
+      // See if there is a match
+      if (state.cart.map(i => i.name).includes(item.name)) {
+        updatedCart = state.cart.map(cartItem => {
+          if (cartItem.name === item.name) return {...cartItem, quantity: cartItem.quantity + 1}
+          return cartItem
+        });
+      } else {
+        updatedCart = [...state.cart, {...item, quantity: 1}]
+      }
+      cartTotal = calcCartTotal(updatedCart);
+      return { ...state, cart: updatedCart, total: cartTotal }
+    }
+    case "INCREASE": 
+      updatedCart = state.cart.map(cartItem => {
+        if (cartItem.name === item.name) return {...cartItem, quantity: cartItem.quantity + 1}
+        return cartItem
+      });
+      cartTotal = calcCartTotal(updatedCart);
+      return { ...state, cart: updatedCart, total: cartTotal }
+
+    case "DECREASE":
+      let newQty;
+      updatedCart = state.cart.map(cartItem => {
+        if (cartItem.name === item.name) {
+          newQty = cartItem.quantity - 1;
+          return {...cartItem, quantity: newQty}
+        }
+        return cartItem
+      });
+      
+      // If action results in 0 quantity, remove item from cart
+      if (newQty === 0) {
+        updatedCart = updatedCart.filter(item => item.quantity > 0);
+      }
+
+      cartTotal = calcCartTotal(updatedCart);
+      return { ...state, cart: updatedCart, total: cartTotal }
+
+    default: 
+      return state
+  }
+}
+
 function ShoppingCart () {
-  const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  const { cart, total } = state;
+
+  const calcItemTotal = item => parseFloat(item.quantity * item.price).toFixed(2);
 
   return (
     <div>
@@ -24,7 +95,7 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={() => dispatch({ type: 'ADD', item})}>Add to Cart</button>
             </div>)
           )}
         </div>
@@ -34,17 +105,17 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button>-</button>
+                <button onClick={() => dispatch({ type: "DECREASE", item})}>-</button>
                 {item.quantity}
-                <button>+</button>
+                <button onClick={() => dispatch({ type: "INCREASE", item})}>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: ${calcItemTotal(item)}</p>
             </div>
           ))}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: ${total}</h2>
       </div>
     </div>
   )
